71867cb0f2ec47e6dd5250590b7d9f747c34b236
kaamil2 <87876889+kaamil2@users.noreply.github.com>
2023-07-23 14:33:51 -0400

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "http://127.0.0.1:4820/openapi.json",
+    url: "https://raw.githubusercontent.com/asyncapi/spec/v2.6.0/examples/streetlights-kafka.yml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
